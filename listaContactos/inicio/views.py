89655c1f63cdaa2c2a49d7b1211436a0b1d57e36
Alejandro G <dgarciat@unsa.edu.pe>
2022-06-16 08:31:28 -0500

    usando variables de un contexto y el tag for para mostrarlos en la vista

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -6,7 +6,12 @@ from django.http import HttpResponse
 def myHomeView(request,*args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return render(request,'home.html')
+    myContext = {
+        'myText' : "esto es sobre nosotros",
+        'myNumber' : 123,
+        'myList' :[33,44,55]
+    }
+    return render(request,'home.html',myContext)
 
 def otraCosaVista(request):
     return HttpResponse('<h1>Otra cosa de contenido<h/1>')
