99f370d2a29e4c7234161763f28ac0ee7c862786
Alejandro G <dgarciat@unsa.edu.pe>
2022-06-16 09:27:07 -0500

    Renderizando una Persona de la app personas

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView, otraCosaVista
+from personas.views import personaTestView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',myHomeView, name='Pagina de inicio'),
     path('otrapagina',otraCosaVista, name='otracosa'),
+    path('persona/', personaTestView, name='Otro'),
 ]
--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Persona
+
 # Create your views here.
+def personaTestView(request):
+    obj = Persona.objects.get(id=1)
+    context = {
+    'objeto' : obj,
+    
+    }
+    return render(request, 'personas/test.html', context) 
