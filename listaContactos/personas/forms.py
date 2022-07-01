f9360d13030c6b3c35e2b9c9f6683f5fd8a2e869
Alejandro G <dgarciat@unsa.edu.pe>
2022-06-30 22:23:41 -0500

    usamos lógica de validación personalizada, tambien se usa widgets y la validación extra de campos

--- listaContactos/personas/forms.py
+++ listaContactos/personas/forms.py
@@ -10,9 +10,26 @@ class PersonaForm(forms.ModelForm):
             'edad',
             'donador',
         ]
+    def clean_nombres(self,*arg,**kwargs):
+        print('paso a la limpieza del campo de nombres')   
+        name=self.cleaned_data.get('nombres')
+        if name.istitle():
+            return name
+        else: 
+            raise forms.ValidationError('la primara letra en mayuscula')
 
 class RawPersonaForm(forms.Form):
-    nombres=forms.CharField()
+    nombres=forms.CharField(
+        widget = forms.Textarea(
+            attrs={
+                'placeholder' : 'solo ingresa tu nombre',
+                'id' : 'nombreID',
+                'class' : 'special',
+                'cols' : '20',
+                'rows' : '5'
+            }
+        )
+    )
     apellidos=forms.CharField()
-    edad=forms.IntegerField()
+    edad=forms.IntegerField(initial=20)
     donador=forms.BooleanField()
